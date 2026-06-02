014338af729843f8e269f445687c53927c05b6a3
NguyenDThai <thainguyen4646@gmail.com>
2026-06-02 10:34:18 +0700

    feat: protected, public route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,32 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "@/src/pages/home/Home";
 import Login from "@/src/pages/login/Login";
 import Register from "@/src/pages/register/Register";
+import PublicRoute from "@/src/routes/PublicRoute";
+import ProtectedRoute from "@/src/routes/ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
   },
   {
     path: "/login",
-    element: <Login />,
+    element: (
+      <PublicRoute>
+        <Login />
+      </PublicRoute>
+    ),
   },
   {
     path: "/register",
-    element: <Register />,
+    element: (
+      <PublicRoute>
+        <Register />
+      </PublicRoute>
+    ),
   },
 ]);
